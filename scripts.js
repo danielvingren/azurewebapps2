1bbabe25da2b6960093c0267c6942856bf83187a
danielvingren <Daniel.vingren@tietoevry.com>
2022-12-19 11:32:57 +0100

    lol213

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
-<script type="text/javascript">
+<><script type="text/javascript">
     alert("This alert box was called with the onload event");
 </script>
+
+<script>
+        let joke = "Crome on, catch up!";
+        document.getElementById("greeting").innerHTML = joke;
+</script></>
